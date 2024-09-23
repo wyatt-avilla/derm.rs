a86186dc88aaf2a4073a59f4002f182dba8f3219
wyatt-avilla <wyattmurphy1@gmail.com>
2024-09-23 15:03:53 -0700

    function location

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,28 @@ use font_kit::handle::Handle;
 
 use image::{GenericImageView, Pixel};
 
+fn print_image(path_str: &str) -> Result<(), Box<dyn Error>> {
+    let img = image::open(path_str)?;
+    println!("dimensions {:?}x{:?}", img.width(), img.height());
+
+    for (i, (_x, _y, channels)) in img.grayscale().pixels().enumerate() {
+        let rgb = channels.to_rgb();
+        let pixel_value = rgb.channels()[0];
+
+        if pixel_value > 245 {
+            print!(" . ");
+        } else {
+            print!("{pixel_value}");
+        }
+
+        if i % img.width() as usize == 0 {
+            println!();
+        }
+    }
+
+    Ok(())
+}
+
 fn print_char(font: &Font, glyph_id: u32, canvas_size: u16) -> Result<(), Box<dyn Error>> {
     let mut canvas = Canvas::new(Vector2I::splat(i32::from(canvas_size)), Format::A8);
 
@@ -33,28 +55,6 @@ fn print_char(font: &Font, glyph_id: u32, canvas_size: u16) -> Result<(), Box<dy
         }
     }
     println!();
-
-    Ok(())
-}
-
-fn print_image(path_str: &str) -> Result<(), Box<dyn Error>> {
-    let img = image::open(path_str)?;
-    println!("dimensions {:?}x{:?}", img.width(), img.height());
-
-    for (i, (_x, _y, channels)) in img.grayscale().pixels().enumerate() {
-        let rgb = channels.to_rgb();
-        let pixel_value = rgb.channels()[0];
-
-        if pixel_value > 245 {
-            print!(" . ");
-        } else {
-            print!("{pixel_value}");
-        }
-
-        if i % img.width() as usize == 0 {
-            println!();
-        }
-    }
 
     Ok(())
 }

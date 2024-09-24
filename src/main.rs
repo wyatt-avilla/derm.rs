543c5ba376e1e8dd8a451205d940222fd1167d4c
wyatt-avilla <wyattmurphy1@gmail.com>
2024-09-23 19:31:18 -0700

    image partition function

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,61 @@ use pathfinder_geometry::vector::{Vector2F, Vector2I};
 
 use font_kit::handle::Handle;
 
-use image::{GenericImageView, Pixel};
+use image::{DynamicImage, GenericImageView, Pixel, SubImage};
+
+use std::vec::Vec;
+
+// row major traversal
+fn img_partitions_from(
+    img: &DynamicImage,
+    partition_width: u32,
+    partition_height: u32,
+    keep_partial_partitions: bool,
+) -> Vec<SubImage<&DynamicImage>> {
+    let (img_width, img_height) = img.dimensions();
+
+    if partition_width >= img_width || partition_height >= img_height {
+        return vec![img.view(0, 0, img_width, img_height)];
+    }
+
+    let partition_start_coordinates = (0..img_height)
+        .step_by(partition_height as usize)
+        .flat_map(|y| {
+            (0..img_width)
+                .step_by(partition_width as usize)
+                .map(move |x| (x, y))
+        })
+        .filter(|&(x, y)| {
+            keep_partial_partitions
+                || (img_width - x >= partition_width && img_height - y >= partition_height)
+        });
+
+    partition_start_coordinates
+        .into_iter()
+        .filter_map(|(x, y)| {
+            if x + partition_width <= img_width && y + partition_height <= img_height {
+                return Some(img.view(x, y, partition_width, partition_height));
+            }
+
+            if keep_partial_partitions {
+                let truncated_width = if x + partition_width > img_width {
+                    img_width - x
+                } else {
+                    partition_width
+                };
+                let truncated_height = if y + partition_height > img_height {
+                    img_height - y
+                } else {
+                    partition_height
+                };
+
+                return Some(img.view(x, y, truncated_width, truncated_height));
+            }
+
+            None
+        })
+        .collect()
+}
 
 fn print_image(path_str: &str) -> Result<(), Box<dyn Error>> {
     let img = image::open(path_str)?;
@@ -87,7 +141,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     print_char(&hask, hask.glyph_for_char('█').unwrap(), 44)?;
     print_char(&jp, jp.glyph_for_char('漢').unwrap(), 44)?;
 
-    print_image("src/images/smiley.png")?;
+    let test_img_path = "src/images/smiley.png";
+
+    print_image(test_img_path)?;
+
+    let img = image::open(test_img_path)?;
+    let sub_images = img_partitions_from(&img, 25, 25, false);
 
     Ok(())
 }

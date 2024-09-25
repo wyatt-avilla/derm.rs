176a5f0869f720a971c9eb1aafdab2a34657d02f
wyatt-avilla <wyattmurphy1@gmail.com>
2024-09-25 12:12:52 -0700

    switch font library

--- src/font_utils.rs
+++ src/font_utils.rs
@@ -1,19 +1,6 @@
-use font_kit::font::Font;
-use font_kit::handle::Handle;
-use font_kit::source::SystemSource;
+use fontdue::Font;
 use std::error::Error;
 
 pub fn search_for_font(family_name: &str, font_name_pattern: &str) -> Result<Font, Box<dyn Error>> {
-    let src = SystemSource::new();
-    let fam = src.select_family_by_name(family_name)?;
-    Ok(fam
-        .fonts()
-        .iter()
-        .find(|h| {
-            matches!(h, Handle::Path { path, .. } if path
-                .to_str()
-                .map_or(false, |name| name.contains(font_name_pattern)))
-        })
-        .ok_or("unable to find font in system")?
-        .load()?)
+    todo![]
 }
--- src/main.rs
+++ src/main.rs
@@ -6,37 +6,20 @@ mod visualize;
 use font_utils::search_for_font;
 use image_utils::img_partitions_from;
 
-use font_kit::canvas::{Canvas, Format, RasterizationOptions};
-use font_kit::hinting::HintingOptions;
+use fontdue::Font;
 use image::{GenericImageView, Pixel};
-use pathfinder_geometry::transform2d::Transform2F;
-use pathfinder_geometry::vector::{Vector2F, Vector2I};
 use std::error::Error;
 use visualize::print_to_console;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let hask = search_for_font(
-        "Hasklug Nerd Font Mono",
-        "Hasklug Medium Nerd Font Complete Mono",
-    )?;
+    let font_data = std::fs::read("/usr/share/fonts/noto-cjk/NotoSansCJK-Regular.ttc")?;
 
-    let jp = search_for_font("Noto Serif CJK JP", "Regular")?;
-    println!("{} contains {} chars", hask.full_name(), hask.glyph_count());
-    println!("{} contains {} chars", jp.full_name(), jp.glyph_count());
+    let jp = Font::from_bytes(font_data, fontdue::FontSettings::default())?;
+    let (metrics, bitmap) = jp.rasterize('漢', 44.0);
 
-    let canvas_size: u16 = 44;
-    let mut canvas = Canvas::new(Vector2I::splat(i32::from(canvas_size)), Format::A8);
-    let vert_trans_fact = f32::from(canvas_size) * 0.10;
-    jp.rasterize_glyph(
-        &mut canvas,
-        jp.glyph_for_char('漢').unwrap(),
-        f32::from(canvas_size),
-        Transform2F::from_translation(Vector2F::new(0.0, f32::from(canvas_size) - vert_trans_fact)),
-        HintingOptions::None,
-        RasterizationOptions::GrayscaleAa,
-    )?;
+    println!("font in use: {}", jp.name().expect("font has no name"));
 
-    print_to_console(&canvas.pixels.iter(), canvas.stride, |&x| x > 100);
+    print_to_console(&bitmap.iter(), metrics.width, |&x| x > 100);
 
     let img = image::open("src/images/smiley.png")?;
     let _sub_images = img_partitions_from(&img, 25, 25, false);

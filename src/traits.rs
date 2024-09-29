2c7eeb96a4458293b7a3f69153aed2217edf6bf3
wyatt-avilla <wyattmurphy1@gmail.com>
2024-09-29 12:59:18 -0700

    result types and nice errors for traits

--- src/traits.rs
+++ src/traits.rs
@@ -1,20 +1,35 @@
-use std::error::Error;
+type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug, PartialEq)]
+pub enum Error {
+    RowSize(usize),
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::RowSize(i) => write!(f, "Invalid Row Size: {i}"),
+        }
+    }
+}
+
+impl std::error::Error for Error {
+    fn description(&self) -> &str {
+        match self {
+            Error::RowSize(_) => "Invalid Row Size",
+        }
+    }
+}
 
 // TODO: change this
 pub trait Pointify {
-    fn to_points(
-        &self,
-        row_size: usize,
-    ) -> Result<impl Iterator<Item = (u16, u16, u16)>, Box<dyn Error>>;
+    fn to_points(&self, row_size: usize) -> Result<impl Iterator<Item = (u16, u16, u16)>>;
 }
 
 impl Pointify for Vec<u8> {
-    fn to_points(
-        &self,
-        row_size: usize,
-    ) -> Result<impl Iterator<Item = (u16, u16, u16)>, Box<dyn Error>> {
+    fn to_points(&self, row_size: usize) -> Result<impl Iterator<Item = (u16, u16, u16)>> {
         if u16::try_from(row_size).is_err() || row_size == 0 {
-            return Err(String::from("invalid row size").into());
+            return Err(Error::RowSize(row_size));
         }
 
         Ok(self.chunks(row_size).enumerate().flat_map(|(col, chunk)| {

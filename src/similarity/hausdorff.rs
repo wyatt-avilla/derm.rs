4a08f37e89a9267f372f4ca4603bd1d0c885f887
wyatt-avilla <wyattmurphy1@gmail.com>
2024-09-29 10:58:06 -0700

    point usage in hausdorff

--- src/similarity/hausdorff.rs
+++ src/similarity/hausdorff.rs
@@ -1,15 +1,15 @@
-use std::collections::HashSet;
+use super::Points;
 
 fn euclidean_distance(x: (u16, u16), y: (u16, u16)) -> f64 {
     f64::from((i32::from(x.0) - i32::from(y.0)).pow(2) + (i32::from(x.1) - i32::from(y.1)).pow(2))
         .sqrt()
 }
 
-pub fn hausdorff_distance(sx: &HashSet<(u16, u16)>, sy: &HashSet<(u16, u16)>) -> f32 {
-    sx.iter()
+pub fn hausdorff_distance(a: &Points, b: &Points) -> f32 {
+    a.iter()
         .map(|p_1| {
             #[allow(clippy::cast_possible_truncation)]
-            sy.iter()
+            b.iter()
                 .map(|p_2| euclidean_distance(*p_1, *p_2) as f32)
                 .fold(f32::INFINITY, f32::min)
         })
